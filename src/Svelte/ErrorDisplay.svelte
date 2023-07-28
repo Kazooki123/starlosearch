323dbae94241dab97fd9064d18613a27a9359bd6
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-07-28 13:56:04 +0800

    Add files via upload
    Source Folder.

---
+++ src/Svelte/ErrorDisplay.svelte
@@ -0,0 +1,20 @@
+<!-- ErrorDisplay.svelte -->
+
+<script>
+    export let message;
+  </script>
+  
+  <style>
+    .error {
+      color: red;
+      font-size: 16px;
+      font-weight: bold;
+    }
+  </style>
+  
+  <main>
+    {#if message}
+      <div class="error">{message}</div>
+    {/if}
+  </main>
+  
