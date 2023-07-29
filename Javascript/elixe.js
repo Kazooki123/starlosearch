76038f99895ade7aae7fc342f4c5bcdd9119e3f3
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-07-29 14:47:06 +0800

    Update elixe.js

--- Javascript/elixe.js
+++ Javascript/elixe.js
@@ -1,7 +1,10 @@
+// If you wanna use my Search engine and don't wanna use this, Simply delete it if you want to.
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const TOKEN = 'MTEzMjIzNTEyMjg2Mzc2NzYyMw.G330_L.SxtQ9pEjaxBB7eYxmGldfY7JqA50nTDlL6H0jw';
+// Replace 'YOUR_DISCORD_BOT_TOKEN' to your actual Discord bot token in the Discord developer portal.
+// If you don't have a Discord bot yet simply create one.
+const TOKEN = 'YOUR_DISCORD_TOKEN';
 
 client.once('ready', () => {
   console.log('Elixe is online!');
