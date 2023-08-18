d050346c210c4b12ad7d20c682d9df69c15cc01a
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-08-18 18:44:57 +0800

    Update redis.js
    Updated file of redis.js

--- src/Javascript/redis.js
+++ src/Javascript/redis.js
@@ -2,9 +2,12 @@
 const redis = require('redis');
 
 // Replace the following values with your actual Redis Insight connection details
-const redisHost = 'redis-19035.c302.asia-northeast1-1.gce.cloud.redislabs.com';
-const redisPort = 19035;
-const redisPassword = 'hDXBdzvuWmByNNkuhYqfg4khLZACzma5';
+const redisHost = 'REDIS_HOST';
+const redisPort = REDIS_PORT;
+const redisPassword = 'REDIS_PASSWORD';
+// Replace REDIS_HOST with your host url that can be found in your Redis cloud database
+// Replace REDIS_PORT to your port, example: 14322
+// Replace REDIS_PASSWORD with your actual Redis password, I'll keep mine as a secret.
 
 const client = createClient({
   host: redisHost,
