1642f81a4ab9dfe75c8208203850a7bbb6b93e90
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-08-18 18:53:26 +0800

    Update redis.py

--- src/Python/redis.py
+++ src/Python/redis.py
@@ -1,6 +1,6 @@
 import redis
 
 r = redis.Redis(
-  host='redis-19035.c302.asia-northeast1-1.gce.cloud.redislabs.com',
-  port=19035,
-  password='hDXBdzvuWmByNNkuhYqfg4khLZACzma5')
+  host='REDIS_HOST',
+  port=YOUR_PORT,
+  password='REDIS_PASSWORD')
