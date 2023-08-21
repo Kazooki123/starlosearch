138488bbd00be695fe8f28d53f3a4c957c8544fe
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-08-21 17:56:45 +0800

    New files
    New files for the project that adds 
    -Search History(uses browser local storage)
    -Login/Sign up page
    -And a webpack incase of http/https request if applicable.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    fallback: {
+      util: require.resolve('util/')
+    }
+  }
+};
+  
