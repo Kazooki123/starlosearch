10705ae9915f1df8a015acb279398c78e70f4264
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-10-28 21:13:56 +0800

    Delete src directory
    Delete src directory to store a new src one

--- src/Java/MultiThreading/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int numThreads = 3; // You can adjust the number of threads as needed
-
-        ThreadManager threadManager = new ThreadManager(numThreads);
-        threadManager.startThreads();
-    }
-}
--- src/Python/redis.py
+++
@@ -1,6 +0,0 @@
-import redis
-
-r = redis.Redis(
-  host='REDIS_HOST',
-  port=YOUR_PORT,
-  password='REDIS_PASSWORD')
