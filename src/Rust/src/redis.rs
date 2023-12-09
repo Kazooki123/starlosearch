32aeaad7a7a9509a887cce892715bc89a040cd2a
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-12-09 18:03:49 +0800

    feat(rust): Add Redis functionality to the project
    This commit adds Redis functionality to the Rust project. It includes the addition of the `redis.rs` file, which contains code for performing operations on Redis. The code sets a key-value pair, gets the value of the key, deletes the key, and checks if the key exists. Redis connections are established using the `redis` crate. This functionality enhances the project's data storage capabilities and allows interaction with a Redis server.
    
    Implemented the following changes:
    - Added the `redis.rs` file, which contains Redis code for performing operations.
    - Imported the `redis` crate.
    - Created a client object and established a connection to a Redis server.
    - Implemented functions for setting a key-value pair, getting the value of a key, deleting a key, and checking if a key exists.
    - Handled errors and printed the results of the operations.

---
+++ src/Rust/src/redis.rs
@@ -0,0 +1,43 @@
+// Import the redis crate
+extern crate redis;
+
+// Use the redis::Commands trait
+use redis::Commands;
+
+// Define a function that performs some operations on Redis
+fn do_something() -> redis::RedisResult<()> {
+    // Create a client object
+    let client = redis::Client::open("redis-10875.c302.asia-northeast1-1.gce.cloud.redislabs.com")?;
+
+    // Get a connection from the client
+    let mut con = client.get_connection()?;
+
+    // Set a key-value pair
+    let _: () = con.set("HELLO", "HIJACKED")?;
+
+    // Get the value of the key
+    let value: String = con.get("HELLO")?;
+
+    // Print the value
+    println!("The value of my_key is {}", value);
+
+    // Delete the key
+    let _: () = con.del("HELLO")?;
+
+    // Check if the key exists
+    let exists: bool = con.exists("HELLO")?;
+
+    // Print the result
+    println!("Does HELLO exist? {}", exists);
+
+    // Return Ok(())
+    Ok(())
+}
+
+// Call the function and handle errors
+fn main() {
+    match do_something() {
+        Ok(_) => println!("Done"),
+        Err(e) => println!("Error: {}", e),
+    }
+}
