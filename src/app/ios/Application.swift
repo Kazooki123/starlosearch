806ccae565970b6ed7663ff5e527f9f1c27bdb9d
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-12-15 14:56:35 +0800

    Added Applications scripts.

---
+++ src/app/ios/Application.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Application: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct Application_Previews: PreviewProvider {
+    static var previews: some View {
+        Application()
+    }
+}
