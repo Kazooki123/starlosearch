18528f60f5e027976269812059a1c32c1af137a7
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-12-16 13:41:48 +0800

    Added backend python for machine learning. Will be updated sooner or later.

---
+++ src/app/mobile/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to my App!'),
+      ),
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+    );
+  }
+}
