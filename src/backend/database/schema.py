6950b38e263a5b771a2bc0852e7dcb234886574d
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2023-12-22 19:56:58 +0800

    Added sign-up/ including the react app for sign in functions.

---
+++ src/backend/database/schema.py
@@ -0,0 +1,20 @@
+import asyncio
+from prisma import Prisma
+from prisma.models import User
+
+async def main() -> None:
+    db = Prisma(auto_register=True)
+    await db.connect()
+
+    # write your queries here
+    user = await User.prisma().create(
+        data={
+            'name': 'Robert',
+            'email': 'robert@craigie.dev'
+        },
+    )
+
+    await db.disconnect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
