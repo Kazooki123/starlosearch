70ce62191c2d4e17d0cd63c08257ef56f31f8b5a
KAZOOKIovestocode <139462470+Kazooki123@users.noreply.github.com>
2024-03-08 17:23:48 +0800

    Updates #20 w/ new HTML5 files!

---
+++ src/frontend/clerk-service.js
@@ -0,0 +1,15 @@
+// clerk-service.js
+import Clerk from '@clerk/clerk-js';
+
+const clerkPublishableKey = `pk_test_cGxlYXNpbmctcmVpbmRlZXItNTEuY2xlcmsuYWNjb3VudHMuZGV2JA`;
+
+async function loadClerk() {
+  const clerk = new Clerk(clerkPublishableKey);
+  await clerk.load({
+    // Assuming you want to redirect to 'load-clerk.html' after sign-in/sign-up
+    afterSignInUrl: 'https://starlosearch.vercel.app/index.html',
+    afterSignUpUrl: 'https://starlosearch.vercel.app/index.html'
+  });
+}
+
+loadClerk();
